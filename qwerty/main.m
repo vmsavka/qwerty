22820cea29434342f0505f72f67c16d24c2c3920
Сергей <brt@serj-mackbook.training.local>
2014-07-31 13:18:30 +0200

    because of!

--- qwerty/main.m
+++ qwerty/main.m
@@ -13,7 +13,7 @@
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        NSLog(@"sagjl");
+        NSLog(@"YAYAYAYAY!");
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
     }
 }
